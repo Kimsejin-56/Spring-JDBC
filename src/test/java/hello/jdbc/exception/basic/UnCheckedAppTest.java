a5102cc169d45f11e11095a40957943dc912437e
Kimsejin-56 <saj5877@naver.com>
2026-03-11 12:42:47 +0900

    언체크 예외 활용

---
+++ src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -0,0 +1,76 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+/**
+ * 2가지 문제
+ * 1. 복구 불가능한 예외 해결(처리 못하는 예외 무시)
+ * 2. 의존 관계 문제 해결(처리 못하면 무시하기에 기술이 변경되어도 controller, service 코드 수정 X)
+ */
+@Slf4j
+public class UnCheckedAppTest {
+
+    @Test
+    void unChecked() {
+        Controller controller = new Controller();
+        Assertions.assertThatThrownBy(() -> controller.request())
+                .isInstanceOf(Exception.class);
+    }
+
+    //예외 처리 못해서 밖으로 던짐
+    static class Controller{
+        Service service = new Service();
+
+        public void request() {
+            service.logic();
+        }
+    }
+
+    //예외 처리 못해서 밖으로 던짐
+    static class Service{
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        public void logic() {
+            repository.call();
+            networkClient.call();
+        }
+    }
+
+    static class Repository{
+        public void call() {
+            try {
+                runSQL();
+            } catch (SQLException e) { //checkException -> unCheckException 전환
+                throw new RuntimeSQLException(e);
+            }
+        }
+
+        private void runSQL() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+
+    static class NetworkClient{
+        public void call() {
+            throw new RuntimeConnectException("연결 실패");
+        }
+    }
+
+    static class RuntimeConnectException extends RuntimeException {
+        public RuntimeConnectException(String message) {
+            super(message);
+        }
+    }
+
+    static class RuntimeSQLException extends RuntimeException {
+        public RuntimeSQLException(Throwable cause) {
+            super(cause);
+        }
+    }
+}
